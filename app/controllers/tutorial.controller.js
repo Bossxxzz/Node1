fa6d8bc20f610422ca87ccee26eaf701c94e180a
Bossxxzz <fatonah.yuso@northbkk.ac.th>
2023-08-29 17:36:18 +0700

    first commit

---
+++ app/controllers/tutorial.controller.js
@@ -0,0 +1,39 @@
+const db = require("../model");
+const Tutorial = db.tutorials;
+const Op = db.Sequelize.Op; 
+
+exports.create = (req, res) => {
+};
+
+exports.findAll = (req, res) => {
+    const title = req.body.title;
+    var condition = title ? {title: {[Op.like]: `%${title}%`}} : null;
+
+    Tutorial.findAll({ where : condition })
+    .then(data => {
+        res.send(data);
+
+    })
+    .catch(err => {
+        res.status(500).send({
+            messaga: err.message || " Some Error occurre! "
+        });      
+    });
+};
+
+exports.findOne = (req, res) => {
+};
+
+exports.findAllPublished = (req, res) => {
+};
+exports.update = (req, res) => {
+    
+};
+
+exports.delete = (req, res) => {
+    
+};
+
+exports.deleteAll = (req, res) => {
+    
+};
